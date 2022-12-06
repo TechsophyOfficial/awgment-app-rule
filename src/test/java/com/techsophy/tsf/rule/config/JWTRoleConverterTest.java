690ae17674630844e001a5418b710f179ea01b9b
Rajeev <rajeevkumar.s@techsophy.com>
2022-12-06 13:51:31 +0530

    Code Coverage is increased to 85%

--- src/test/java/com/techsophy/tsf/rule/config/JWTRoleConverterTest.java
+++ src/test/java/com/techsophy/tsf/rule/config/JWTRoleConverterTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
 import javax.servlet.http.HttpServletRequest;
+import java.nio.file.AccessDeniedException;
 import java.time.Instant;
 import java.util.*;
 import static com.techsophy.tsf.rule.constants.RuleTestConstants.GET;
@@ -54,7 +56,48 @@ class JWTRoleConverterTest
                 .thenReturn("abc");
         when(mockObjectMapper.readValue("abc",Map.class)).thenReturn(map);
         when(mockObjectMapper.convertValue(any(),eq(List.class))).thenReturn(list);
-        when(tokenUtils.getIssuerFromToken(anyString())).thenReturn("techsophy-platform");
+        Collection grantedAuthority =  jwtRoleConverter.convert(jwt);
+        Assertions.assertNotNull(grantedAuthority);
+    }
+
+    @Test
+    void convertTestWhileThrowingException() throws JsonProcessingException {
+        Jwt jwt = Mockito.mock(Jwt.class);
+        String userResponce = "";
+        Mockito.when(webClientWrapper.webclientRequest(any(), any(), any(), any())).thenReturn(userResponce);
+        Mockito.when(tokenUtils.getIssuerFromToken(anyString())).thenReturn("techsophy-platform");
+        Assertions.assertThrows(AccessDeniedException.class, () -> jwtRoleConverter.convert(jwt));
+    }
+
+    @Test
+    void convertTestWhileRoleListIsEmpty() throws JsonProcessingException
+    {
+        Map<String, Object> map = new HashMap<>();
+        map.put("clientRoles", "abc");
+        List<String> list=new ArrayList<>();
+        Jwt jwt= new Jwt("1",Instant.now(),null,Map.of("abc","abc"),Map.of("abc","abc"));
+        WebClient webClient= WebClient.builder().build();
+        when(webClientWrapper.createWebClient(any())).thenReturn(webClient);
+        when(webClientWrapper.webclientRequest(any(),any(),eq(GET),any()))
+                .thenReturn("abc");
+        when(mockObjectMapper.readValue("abc",Map.class)).thenReturn(map);
+        when(mockObjectMapper.convertValue(any(),eq(List.class))).thenReturn(list);
+        Collection grantedAuthority =  jwtRoleConverter.convert(jwt);
+        Assertions.assertNotNull(grantedAuthority);
+    }
+
+    @Test
+    void convertTestWhileUserInformationMapIsEmpty() throws JsonProcessingException
+    {
+        Map<String, Object> map = new HashMap<>();
+        List<String> list=new ArrayList<>();
+        Jwt jwt= new Jwt("1",Instant.now(),null,Map.of("abc","abc"),Map.of("abc","abc"));
+        WebClient webClient= WebClient.builder().build();
+        when(webClientWrapper.createWebClient(any())).thenReturn(webClient);
+        when(webClientWrapper.webclientRequest(any(),any(),eq(GET),any()))
+                .thenReturn("abc");
+        when(mockObjectMapper.readValue("abc",Map.class)).thenReturn(map);
+        when(mockObjectMapper.convertValue(any(),eq(List.class))).thenReturn(list);
         Collection grantedAuthority =  jwtRoleConverter.convert(jwt);
         Assertions.assertNotNull(grantedAuthority);
     }

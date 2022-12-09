e3e357b869dd3bce17ec06c24017274d6d3e32d8
Rajeev <rajeevkumar.s@techsophy.com>
2022-12-09 12:23:20 +0530

    Sonar Lint Issues resolved

--- src/main/java/com/techsophy/tsf/rule/aspect/LoggingHandler.java
+++ src/main/java/com/techsophy/tsf/rule/aspect/LoggingHandler.java
@@ -17,46 +17,47 @@ import static com.techsophy.tsf.rule.constants.RuleModelerConstants.*;
 public class LoggingHandler
 {
     Logger logger= LoggerFactory.getLogger(this.getClass());
+    String args = "{}{}";
 
     @Before(CONTROLLER_CLASS_PATH)
     void beforeController(JoinPoint joinPoint)
     {
-    String name= joinPoint.getSignature().getName();
-    logger.info(name+IS_INVOKED_IN_CONTROLLER);
+        String name= joinPoint.getSignature().getName();
+        logger.info(args,name,IS_INVOKED_IN_CONTROLLER);
     }
 
     @After(CONTROLLER_CLASS_PATH)
     void afterController(JoinPoint joinPoint)
     {
-    String name= joinPoint.getSignature().getName();
-    logger.info(name+EXECUTION_IS_COMPLETED_IN_CONTROLLER);
+        String name= joinPoint.getSignature().getName();
+        logger.info(args, name, EXECUTION_IS_COMPLETED_IN_CONTROLLER);
     }
 
     @Before(SERVICE_CLASS_PATH)
     void beforeService(JoinPoint joinPoint)
     {
         String name = joinPoint.getSignature().getName();
-        logger.info(name+IS_INVOKED_IN_SERVICE);
+        logger.info(args, name, IS_INVOKED_IN_SERVICE);
     }
 
     @After(SERVICE_CLASS_PATH)
     void afterService(JoinPoint joinPoint)
     {
         String name = joinPoint.getSignature().getName();
-        logger.info(name+EXECUTION_IS_COMPLETED_IN_SERVICE);
+        logger.info(args, name, EXECUTION_IS_COMPLETED_IN_SERVICE);
     }
 
     @AfterThrowing(value=CONTROLLER_CLASS_PATH,throwing=EXCEPTION)
     public void logAfterThrowingController(JoinPoint joinPoint, Exception ex)
     {
-        logger.error(EXCEPTION_THROWN+joinPoint.getSignature().getName()+BRACKETS_IN_CONTROLLER);
-        logger.error(CAUSE+ex.getMessage());
+        logger.error("{}{}{}", EXCEPTION_THROWN,joinPoint.getSignature().getName(),BRACKETS_IN_CONTROLLER);
+        logger.error(args, CAUSE, ex.getMessage());
     }
 
     @AfterThrowing(value=SERVICE_CLASS_PATH,throwing=EXCEPTION)
     public void logAfterThrowingService(JoinPoint joinPoint, Exception ex)
     {
-        logger.error(EXCEPTION_THROWN+joinPoint.getSignature().getName()+BRACKETS_IN_SERVICE);
-        logger.error(CAUSE+ex.getMessage());
+        logger.error("{}{}{}",EXCEPTION_THROWN,joinPoint.getSignature().getName(),BRACKETS_IN_SERVICE);
+        logger.error(args, CAUSE, ex.getMessage());
     }
 }
